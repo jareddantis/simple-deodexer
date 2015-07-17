e1d17e1443537ebc9203aa71c6dbde1ac5597292
Aurel Jared Dantis <jareddantis@gmail.com>
2015-07-17 10:32:56 +0800

    Optimizations and new features
    * Add zipalign binary for OS X
    * Merge external scripts into deodex.sh
    * Add (long due) support for bootclasspath
    * More comprehensive API level guide - all the way from 1.0 to 5.1.x!
    * Update baksmali 2.0.3 to 2.0.6

--- deodex.sh
+++ deodex.sh
@@ -3,23 +3,67 @@ rootdir=$(pwd)
 chmod 0755 tools/*
 version=$(cat tools/version.txt)
 
-if [[ $(uname -p) == "x86_64" ]] && [[ $(dpkg -l | grep 'libc6-i386') == "" ]] && [[ $(dpkg -l | grep 'ia32-libs') == "" ]]; then
-	if [[ $(lsb_release -r | cut -f 2 | sed -r 's/.{3}$//') -ge "13" ]]; then
-		if [[ $(lsb_release -r | cut -f 2 | sed -r 's/.{3}$//') == "13" ]] && [[ $(lsb_release -r | cut -f 2 | sed -r 's/^.{3}//') == "04" ]]; then
-			echo "ERROR: This script requires 32-bit compatibility packages."
-			echo "To install them, type \"sudo apt-get install ia32-libs\"."
+if [[ $(uname -p) == "x86_64" ]] && [[ $(which lsb_release) != "" ]]; then
+	if [[ $(dpkg -l | grep 'libc6-i386') == "" ]] || [[ $(dpkg -l | grep 'ia32-libs') == "" ]]; then
+		if [[ $(lsb_release -r | cut -f 2 | sed -r 's/.{3}$//') -ge "13" ]]; then
+			if [[ $(lsb_release -r | cut -f 2 | sed -r 's/.{3}$//') == "13" ]] && [[ $(lsb_release -r | cut -f 2 | sed -r 's/^.{3}//') == "04" ]]; then
+				echo "ERROR: This script requires 32-bit compatibility packages."
+				echo "To install them, type \"sudo apt-get install ia32-libs\"."
+			else
+				echo "ERROR: This script requires 32-bit compatibility packages."
+				echo "To install them, type:"
+				echo "\"sudo apt-get install lib32gcc1 libc6-i386 lib32z1 lib32stdc++6 lib32bz2-1.0 lib32ncurses5\"."
+			fi
 		else
 			echo "ERROR: This script requires 32-bit compatibility packages."
-			echo "To install them, type:"
-			echo "\"sudo apt-get install lib32gcc1 libc6-i386 lib32z1 lib32stdc++6 lib32bz2-1.0 lib32ncurses5\"."
+			echo "To install them, type \"sudo apt-get install ia32-libs\"."
 		fi
 	else
-		echo "ERROR: This script requires 32-bit compatibility packages."
-		echo "To install them, type \"sudo apt-get install ia32-libs\"."
+		aligner="tools/zipalign.linux"
 	fi
-	
 	exit 1
+elif [[ $(uname -a | grep -i 'Darwin') != "" ]]; then
+	aligner="tools/zipalign.osx"
+else
+	aligner="tools/zipalign.linux"
 fi
+chmod +x $aligner
+
+showhelp() {
+	echo ""
+	if [[ $1 = "1" ]]; then
+		echo "ERROR: API level not specified."
+	elif [[ $1 == "2" ]]; then
+		echo "ERROR: Invalid option \"$2\"."
+	elif [[ $1 == "3" ]]; then
+		echo "ERROR: No parameters specified."
+	elif [[ $1 == "4" ]]; then
+		echo "ERROR: No options specified."
+	fi
+
+	echo
+	echo "Usage: `basename $0` <options> <API level> [bootclasspath]"
+	echo "        e.g. \"`basename $0` -a 19 :java.obex.jar\""
+	echo "---------------------------------------------------"
+	echo "Options:"
+	echo "    -a       Deodex apps"
+	echo "    -b       Deodex all APKs"
+	echo "    -p       Deodex priv-apps"
+	echo "    -f       Deodex frameworks"
+
+	echo "    -h       Display this help message"
+	echo "    -hh      Display the Android version <> API level guide"
+
+	echo "    -j       Change baksmali/smali version (currently $(cat tools/version.txt))"
+	echo "    -x       Cleanup (delete all files in triage)"
+
+	echo "    -z       Zipalign APKs in all folders"
+	echo "    -zz      Zipalign APKs in app"
+	echo "    -zzz     Zipalign APKs in framework"
+	echo "    -zzzz    Zipalign APKs in priv-app"
+	echo ""
+	exit 0
+}
 
 if [ ! -d triage ]; then
 	mkdir triage
@@ -31,12 +75,12 @@ if [ ! -d triage ]; then
 fi
 
 if [[ ! $1 ]] && [[ ! $2 ]]; then
-	tools/help.sh 3
+	showhelp 3
 	exit 0
 fi
 
 zpln() {
-	$rootdir/tools/zipalign 4 $1 $1-temp
+	$rootdir/$aligner 4 $1 $1-temp
 	rm -f $1
 	mv $1-temp $1
 }
@@ -45,88 +89,67 @@ deodex_file() {
 	echo
 	odex_file=$1
 
-	if [ "$odex_file" == "" ]
-	then
-	  echo "Error: No .odex file specified"
-
-	elif [ -e $odex_file ]
-	then
-	  echo "Working on $odex_file"
-	  echo "- Disassembling $odex_file"
+	if [ "$odex_file" == "" ]; then
+		echo "Error: No .odex file specified"
+	elif [ -e $odex_file ];  then
+	  	echo "Working on $odex_file"
+	  	echo "- Disassembling $odex_file"
 	else
-	  echo "Error: $odex_file not found"
+	  	echo "Error: $odex_file not found"
 	fi
 	
 	# Call baksmali
+	if [[ "$bootclass" != "" ]]; then    # Call baksmali with bootclasspath
+		java -Xmx512m -jar ../../tools/baksmali-$version.jar -a $api_level -d ../framework -c $bootclass -x $odex_file
+		is_error=$?
+	else     # No bootclasspath
+		java -Xmx512m -jar ../../tools/baksmali-$version.jar -a $api_level -d ../framework -x $odex_file
+		is_error=$?
+	fi
 
-	java -Xmx512m -jar ../../tools/baksmali-$version.jar -a $api_level -d ../framework -x $odex_file
-	is_error=$?
-
-	
 	# If there were no errors, then assemble it with smali
+	if [ "$is_error" == "0" ] && [ -d out ]; then
+		echo "- Assembling into classes.dex"
+		java -Xmx512m -jar ../../tools/smali-$version.jar -a $api_level -o classes.dex out
+	  	rm -rf out
 
-	if [ "$is_error" == "0" ] && [ -d out ]
-	then
+		# Ensure classes.dex was produced
+		if [ -e classes.dex ]; then
+			# Ensure the .odex file's .apk or .jar is found
+			no_ext=`echo $odex_file | sed 's/.odex//'`
+			main_file=$no_ext.apk
+			error_found=no
 
-	  echo "- Assembling into classes.dex"
-	  java -Xmx512m -jar ../../tools/smali-$version.jar -a $api_level -o classes.dex out
+			if [ -e $main_file ];then
+				ext=apk
+			else
+				main_file=$no_ext.jar
+			  
+				if [ -e $main_file ]; then
+					ext=jar
+				else          
+					echo "ERROR: Can't find $no_ext.jar or $no_ext.apk"
+					error_found=yes
+			  	fi
+			fi
 
-	  rm -rf out
-	  
-	  # Ensure classes.dex was produced
-	  
-	  if [ -e classes.dex ]
-	  then
+			if [ $error_found == yes ]; then
+			  	echo "- Removing classes.dex"
+			  	rm -f classes.dex
+			else
+			  	echo "- Removing $odex_file"
+			  	rm -f $odex_file
 
-		# Ensure the .odex file's .apk or .jar is found
-		no_ext=`echo $odex_file | sed 's/.odex//'`
-		main_file=$no_ext.apk
-
-		error_found=no
-
-		if [ -e $main_file ]
-		then
-		  ext=apk
-		else
-		  main_file=$no_ext.jar
-		  
-		  if [ -e $main_file ]
-		  then
-			ext=jar
-		  else          
-			echo "ERROR: Can't find $no_ext.jar or $no_ext.apk"
-			error_found=yes
-		  fi
-		fi
-
-
-		if [ $error_found == yes ]
-		then
-
-		  echo "- Removing classes.dex"
-		  rm -f classes.dex
-
-		  # Keep the odex file so that it's left unchanged
-
-		else
-
-		  echo "- Removing $odex_file"
-		  rm -f $odex_file
-
-		  echo "- Putting classes.dex into $main_file"
-		  zip -r -q $main_file classes.dex
-		  rm -f classes.dex
-		  if [ -e $main_file ]
-		  then
-			echo "$main_file has been deodexed"
-		  fi
-		
-		fi
-
-	  else
-		echo "WARNING: Unable to produce classes.dex!"
-	  fi          
-
+			  	echo "- Putting classes.dex into $main_file"
+			  	zip -r -q $main_file classes.dex
+			  	rm -f classes.dex
+			  	if [ -e $main_file ]; then
+					echo "$main_file has been deodexed"
+			  	fi	
+			fi
+	  	else
+			echo "WARNING: Unable to produce classes.dex!"
+	  	fi
 	else
 	  echo "WARNING: Cannot deodex $odex_file"
 	  rm -rf out
@@ -147,13 +170,65 @@ count_odex() {
 	echo $count
 }
 
+changesmali() {
+	echo
+	echo "Current version: $(cat tools/version.txt)"
+	echo "Available versions:"
+	echo "      1  -  1.3.3"
+	echo "      2  -  1.4.2"
+	echo "      3  -  2.0.6"
+	echo ""
+	echo "Choose number corresponding to desired version."
+	echo -n "Leave blank for default (2.0.6): "
+	read choice
+	case $choice in
+		1)
+			echo "1.3.3" > tools/version.txt
+			;;
+		2)
+			echo "1.4.2" > tools/version.txt
+			;;
+		*)
+			echo "2.0.6" > tools/version.txt
+			;;
+	esac
+	echo
+}
+
+apiguide() {
+	echo
+	echo "Android Version    API Level     Codename"
+	echo "--------------------------------------------------------"
+	echo "5.1 - 5.1.1           22         Lollipop"
+	echo "5.0 - 5.0.2           21"
+	echo "4.4W.x                20         KitKat for Android Wear"
+	echo "4.4.x                 19         KitKat"
+	echo "4.3.x                 18         Jellybean"
+	echo "4.2 - 4.2.2           17"
+	echo "4.1 - 4.1.1           16"
+	echo "4.0.3 - 4.0.4         15         Ice Cream Sandwich"
+	echo "4.0 - 4.0.2           14"
+	echo "3.2                   13         Honeycomb"
+	echo "3.1.x                 12"
+	echo "3.0.x                 11"
+	echo "2.3.3 - 2.3.7         10         Gingerbread"
+	echo "2.3 - 2.3.2           9"
+	echo "2.2 - 2.2.3           8          Froyo"
+	echo "2.1                   7          Éclair"
+	echo "2.0.1                 6"
+	echo "2.0                   5"
+	echo "1.6                   4          Donut"
+	echo "1.5                   3          Cupcake"
+	echo "1.1                   2          N/A"
+	echo "1.0                   1          N/A"
+	echo
+}
+
 if [[ ! $1 == "" ]]; then
-	
 	# Help
 	if [ $1 == "-h" ]; then
-		tools/help.sh
+		showhelp
 		exit 0
-		
 	# Deodex
 	elif [ $1 == "-a" ]; then
 		processDirList=(app)
@@ -193,7 +268,6 @@ if [[ ! $1 == "" ]]; then
 		mkdir triage/priv-app
 		echo "Done."
 		exit 0
-	
 	# Zipalign options
 	elif [ $1 == "-z" ]; then
 		printf '\033c'
@@ -233,35 +307,34 @@ if [[ ! $1 == "" ]]; then
 			echo "Zipaligning $f"
 			zpln $f
 		done
-		
 	# Change smali tools version
 	elif [ $1 == "-j" ]; then
-		tools/change.sh
+		changesmali
 		exit 0
 	fi
-
 # No option specified
 elif [[ ! $1 ]] && [[ $2 != "" ]]; then
-	tools/help.sh 4
-
+	showhelp 4
 # Other error
 else
-	tools/help.sh 2 $1
+	showhelp 2 $1
 	exit 0
 fi
-
 # Show API level guide
 if [[ ! $2 ]] && [ $1 == "-hh" ]; then
-	tools/guide.sh
+	showguide
 	exit 0
 elif [[ ! $2 ]] && [[ $1 != "" ]] && [ ! $(echo $1 | grep 'z') ]; then
-	tools/help.sh 1
+	showhelp 1
 	exit 0
 else
 	api_level=$2
 fi
 
 # Process
+if [[ "$3" != "" ]]; then
+	bootclass="$3"
+fi
 for processDir in ${processDirList[@]}; do
 	echo ""
 	echo "Entering $processDir"
