fcb191e5b39fb382eb99e5a68b34b24fb05940b1
Aurel Jared Dantis <jareddantis@gmail.com>
2016-04-23 13:22:25 +0800

    Script overhaul

--- deodex.sh
+++ deodex.sh
@@ -4,88 +4,142 @@ chmod 0755 tools/*
 SAVEIFS=$IFS
 IFS=$'\n'
 
-if [[ $(uname -p) == "x86_64" ]] && [[ $(which lsb_release) != "" ]]; then
-	if [[ $(dpkg -l | grep 'libc6-i386') == "" ]] || [[ $(dpkg -l | grep 'ia32-libs') == "" ]]; then
-		if [[ $(lsb_release -r | cut -f 2 | sed -r 's/.{3}$//') -ge "13" ]]; then
-			if [[ $(lsb_release -r | cut -f 2 | sed -r 's/.{3}$//') == "13" ]] && [[ $(lsb_release -r | cut -f 2 | sed -r 's/^.{3}//') == "04" ]]; then
-				echo "ERROR: This script requires 32-bit compatibility packages."
-				echo "To install them, type \"sudo apt-get install ia32-libs\"."
-			else
-				echo "ERROR: This script requires 32-bit compatibility packages."
-				echo "To install them, type:"
-				echo "\"sudo apt-get install lib32gcc1 libc6-i386 lib32z1 lib32stdc++6 lib32bz2-1.0 lib32ncurses5\"."
-			fi
-		else
-			echo "ERROR: This script requires 32-bit compatibility packages."
-			echo "To install them, type \"sudo apt-get install ia32-libs\"."
-		fi
-	else
-		aligner="tools/zipalign.linux"
-	fi
-	exit 1
+sysApp=0
+privApp=0
+framework=0
+bootclass=
+api=0
+processDirList=0
+custom=0
+
+if [[ $(uname -a | grep -i 'Linux') != "" ]]; then
+	aligner="$rootdir/tools/zipalign.linux"
 elif [[ $(uname -a | grep -i 'Darwin') != "" ]]; then
-	aligner="tools/zipalign.osx"
+	aligner="$rootdir/tools/zipalign.osx"
 else
-	aligner="tools/zipalign.linux"
+	echo "Error: Unsupported operating system."
+	exit 1
 fi
 chmod +x $aligner
-
-showhelp() {
-	echo ""
-	if [[ $1 = "1" ]]; then
-		echo "ERROR: API level not specified."
-	elif [[ $1 == "2" ]]; then
-		echo "ERROR: Invalid option \"$2\"."
-	elif [[ $1 == "3" ]]; then
-		echo "ERROR: No parameters specified."
-	elif [[ $1 == "4" ]]; then
-		echo "ERROR: No options specified."
-	fi
-
-	echo
-	echo "Usage: `basename $0` <options> <API level> [bootclasspath]"
-	echo "        e.g. \"`basename $0` -a 19 :java.obex.jar\""
-	echo "---------------------------------------------------"
-	echo "Options:"
-	echo "    -a       Deodex apps"
-	echo "    -b       Deodex all APKs"
-	echo "    -p       Deodex priv-apps"
-	echo "    -f       Deodex frameworks"
-
-	echo "    -h       Display this help message"
-	echo "    -hh      Display the Android version <> API level guide"
-
-	echo "    -x       Cleanup (delete all files in triage)"
-
-	echo "    -z       Zipalign APKs in all folders"
-	echo "    -zz      Zipalign APKs in app"
-	echo "    -zzz     Zipalign APKs in framework"
-	echo "    -zzzz    Zipalign APKs in priv-app"
-	echo ""
-	exit 0
-}
 
 if [ ! -d triage ]; then
 	mkdir triage
 	cd triage
-	mkdir app
-	mkdir framework
-	mkdir priv-app
+	touch put_apps_here
 	cd ../
 fi
 
-if [[ ! $1 ]] && [[ ! $2 ]]; then
-	showhelp 3
-	exit 0
-fi
-
-zpln() {
-	$rootdir/$aligner 4 $1 $1-temp
-	rm -f $1
-	mv $1-temp $1
+show_api() {
+	echo
+	echo "Android Version    API Level     Codename"
+	echo "--------------------------------------------------------"
+	echo "6.0.x                 23         Marshmallow"
+	echo "5.1 - 5.1.1           22         Lollipop"
+	echo "5.0 - 5.0.2           21"
+	echo "4.4W.x                20         KitKat for Android Wear"
+	echo "4.4.x                 19         KitKat"
+	echo "4.3.x                 18         Jellybean"
+	echo "4.2 - 4.2.2           17"
+	echo "4.1 - 4.1.1           16"
+	echo "4.0.3 - 4.0.4         15         Ice Cream Sandwich"
+	echo "4.0 - 4.0.2           14"
+	echo "3.2                   13         Honeycomb"
+	echo "3.1.x                 12"
+	echo "3.0.x                 11"
+	echo "2.3.3 - 2.3.7         10         Gingerbread"
+	echo "2.3 - 2.3.2           9"
+	echo "2.2 - 2.2.3           8          Froyo"
+	echo "2.1                   7          Éclair"
+	echo "2.0.1                 6"
+	echo "2.0                   5"
+	echo "1.6                   4          Donut"
+	echo "1.5                   3          Cupcake"
+	echo "1.1                   2          N/A"
+	echo "1.0                   1          N/A"
+	echo
 }
 
-deodex_file() {
+show_help() {
+	echo
+	echo "Usage: `basename $0` <options>"
+	echo "        e.g. \"`basename $0` -l 19\""
+	echo "---------------------------------------------------"
+	echo "Options:"
+	echo "    -b         Call baksmali with bootclasspath"
+	echo "    -f <dir>   Only deodex apps in triage/<dir>."
+	echo "    -g         Display API level list"
+	echo "    -h         Display this help message"
+	echo "    -l <num>   Use API level <num>. REQUIRED!"
+	echo "    -z         Only zipalign apps and exit"
+	echo
+}
+
+build_list() {
+	if [[ $sysApp == 1 ]]; then
+		if [[ $privApp == 1 ]]; then
+			if [[ $framework == 1 ]]; then
+				processDirList=(app priv-app framework)
+			else
+				processDirList=(app priv-app)
+			fi
+		else
+			if [[ $framework == 1 ]]; then
+				processDirList=(app framework)
+			else
+				processDirList=(app)
+			fi
+		fi
+	elif [[ $privApp == 1 ]]; then
+		if [[ $framework == 1 ]]; then
+			processDirList=(priv-app framework)
+		else
+			processDirList=(priv-app)
+		fi
+	elif [[ $framework == 1 ]]; then
+		processDirList=(framework)
+	else
+		processDirList=0
+	fi
+}
+
+count_odex() {
+	cd "triage/$1"
+	existence=`ls -l *.odex 2>/dev/null | wc -l`
+	if [ $existence != 0 ]; then
+		for f in $(ls *.odex); do
+			count=$(($count+1))
+		done
+	else
+		count=0
+	fi
+	echo $count
+}
+
+zpln() {
+	"$rootdir/$aligner" 4 "$1" "$1-temp"
+	rm -f "$1"
+	mv "$1-temp" "$1"
+}
+
+zpln_all() {
+	build_list
+	if [[ $processDirList == 0 ]]; then
+		echo "No apps to zipalign."
+		exit 0
+	fi
+
+	cd triage
+	for i in ${processDirList[@]}; do
+		cd "$i"
+		for j in *.apk; do
+			echo "Zipaligning $i/$j"
+			zpln $f
+		done
+		cd ../
+	done
+}
+
+deodex() {
 	echo
 	odex_file=$1
 
@@ -100,17 +154,17 @@ deodex_file() {
 	
 	# Call baksmali
 	if [[ "$bootclass" != "" ]]; then    # Call baksmali with bootclasspath
-		java -Xmx512m -jar ../../tools/baksmali.jar -a $api_level -d ../framework -c $bootclass -x $odex_file
+		java -Xmx512m -jar ../../tools/baksmali.jar -a $api -d ../framework -c $bootclass -x $odex_file
 		is_error=$?
 	else     # No bootclasspath
-		java -Xmx512m -jar ../../tools/baksmali.jar -a $api_level -d ../framework -x $odex_file
+		java -Xmx512m -jar ../../tools/baksmali.jar -a $api -d ../framework -x $odex_file
 		is_error=$?
 	fi
 
 	# If there were no errors, then assemble it with smali
 	if [ "$is_error" == "0" ] && [ -d out ]; then
 		echo "- Assembling into classes.dex"
-		java -Xmx512m -jar ../../tools/smali.jar -a $api_level -o classes.dex out
+		java -Xmx512m -jar ../../tools/smali.jar -a $api -o classes.dex out
 	  	rm -rf out
 
 		# Ensure classes.dex was produced
@@ -156,181 +210,88 @@ deodex_file() {
 	fi
 }
 
-count_odex() {
-	cd triage/$1
-
-	existence=`ls -1 *.odex 2>/dev/null | wc -l`
-	if [ $existence != 0 ]; then
-		for f in $(ls *.odex); do
-			count=$(($count+1))
-		done
-	else
-		count=0
-	fi
-	echo $count
-}
-
-apiguide() {
-	echo
-	echo "Android Version    API Level     Codename"
-	echo "--------------------------------------------------------"
-	echo "6.0.x                 23         Marshmallow"
-	echo "5.1 - 5.1.1           22         Lollipop"
-	echo "5.0 - 5.0.2           21"
-	echo "4.4W.x                20         KitKat for Android Wear"
-	echo "4.4.x                 19         KitKat"
-	echo "4.3.x                 18         Jellybean"
-	echo "4.2 - 4.2.2           17"
-	echo "4.1 - 4.1.1           16"
-	echo "4.0.3 - 4.0.4         15         Ice Cream Sandwich"
-	echo "4.0 - 4.0.2           14"
-	echo "3.2                   13         Honeycomb"
-	echo "3.1.x                 12"
-	echo "3.0.x                 11"
-	echo "2.3.3 - 2.3.7         10         Gingerbread"
-	echo "2.3 - 2.3.2           9"
-	echo "2.2 - 2.2.3           8          Froyo"
-	echo "2.1                   7          Éclair"
-	echo "2.0.1                 6"
-	echo "2.0                   5"
-	echo "1.6                   4          Donut"
-	echo "1.5                   3          Cupcake"
-	echo "1.1                   2          N/A"
-	echo "1.0                   1          N/A"
-	echo
-}
-
-if [[ ! $1 == "" ]]; then
-	# Help
-	if [ $1 == "-h" ]; then
-		showhelp
-		exit 0
-	# Deodex
-	elif [ $1 == "-a" ]; then
-		processDirList=(app)
-		printf '\033c'
-		echo "$(count_odex app) odex files are in /app."
-	elif [ $1 == "-b" ]; then
-		processDirList=(app framework)
-		printf '\033c'
-		echo "$(count_odex app) odex files are in /app."
-		echo "$(count_odex framework) odex files are in /framework."
-		if [ -d triage/priv-app ]; then
-			existence2=`ls -1 triage/priv-app/*.odex 2>/dev/null | wc -l`
-			if [ $existence2 != 0 ]; then
-				processDirList=(app framework priv-app)
-				echo "$existence2 odex files are in /priv-app."
+while getopts "b:hgzl:f:" opt; do
+	case "$opt" in
+		b)
+			bootclass="$rootdir/tools/$OPTARG"
+			;;
+		f)
+			custom=1
+			if [[ -d "triage/$OPTARG" ]] && [[ "$(count_odex $OPTARG)" != 0 ]]; then
+				processDirList=("$OPTARG")
+			else
+				echo "Invalid folder \"$OPTARG\" specified."
+				exit 1
 			fi
-		fi
-	elif [ $1 == "-f" ]; then
-		processDirList=(framework)
-		printf '\033c'
-		echo "$(count_odex framework) odex files are in /framework."
-		cp tools/java.awt.jar triage/framework/java.awt.jar
-	elif [ $1 == "-p" ]; then
-		processDirList=(priv-app)
-		printf '\033c'
-		echo "$(count_odex priv-app) odex files are in /priv-app."
-	elif [ $1 == "-x" ]; then
-		printf '\033c'
-		echo "Cleaning up"
-		echo "- app"
-		rm -fR triage/app
-		mkdir triage/app
-		echo "- framework"
-		rm -fR triage/framework
-		mkdir triage/framework
-		echo "- priv-app"
-		rm -fR triage/priv-app
-		mkdir triage/priv-app
-		echo "Done."
+			;;
+		h)
+			show_help
+			exit 0
+			;;
+		g)
+			show_api
+			exit 0
+			;;
+		z)
+			if [[ -d "triage/app" ]] && [[ "$(count_odex app)" != 0 ]]; then sysApp=1; fi
+			if [[ -d "triage/priv-app" ]] && [[ "$(count_odex priv-app)" != 0 ]]; then privApp=1; fi
+			if [[ -d "triage/framework" ]] && [[ "$(count_odex framework)" != 0 ]]; then framework=1; fi
+			zpln_all
+			IFS=$SAVEIFS
+			exit 0
+			;;
+		l)
+			api="$OPTARG"
+			;;
+		\?)
+			echo "Invalid option -\"$OPTARG\"."
+			echo
+			show_help
+			exit 1
+			;;
+		:)
+			echo "Option -\"$OPTARG\" requires an argument."
+			echo
+			show_help
+			exit 1
+			;;
+	esac
+done
+
+# Check for apps to deodex
+if [[ $custom == 0 ]]; then
+	if [[ -d "triage/app" ]] && [[ "$(count_odex app)" != 0 ]]; then sysApp=1; fi
+	if [[ -d "triage/priv-app" ]] && [[ "$(count_odex priv-app)" != 0 ]]; then privApp=1; fi
+	if [[ -d "triage/framework" ]] && [[ "$(count_odex framework)" != 0 ]]; then framework=1; fi
+	build_list
+	if [[ $processDirList == 0 ]]; then
+		echo "No apps to deodex."
 		exit 0
-	# Zipalign options
-	elif [ $1 == "-z" ]; then
-		printf '\033c'
-		processDirList=(app framework)
-		if [ -d triage/priv-app ]; then
-			existence2=`ls -1 triage/priv-app/*.apk 2>/dev/null | wc -l`
-			if [ $existence2 != 0 ]; then
-				processDirList=(app framework priv-app)
-			fi
-		fi
-		
-		for processDir in ${processDirList[@]}; do
-			cd triage/$processDir
-			for f in $(ls *.apk); do
-				echo "Zipaligning $f"
-				zpln $f
-			done
-		done
-	elif [ $1 == "-zz" ]; then
-		printf '\033c'
-		cd triage/app
-		for f in $(ls *.apk); do
-			echo "Zipaligning $f"
-			zpln $f
-		done
-	elif [ $1 == "-zzz" ]; then
-		printf '\033c'
-		cd triage/framework
-		for f in $(ls *.apk); do
-			echo "Zipaligning $f"
-			zpln $f
-		done
-	elif [ $1 == "-zzzz" ]; then
-		printf '\033c'
-		cd triage/priv-app
-		for f in $(ls *.apk); do
-			echo "Zipaligning $f"
-			zpln $f
-		done
 	fi
-# No option specified
-elif [[ ! $1 ]] && [[ $2 != "" ]]; then
-	showhelp 4
-# Other error
-else
-	showhelp 2 $1
-	exit 0
-fi
-# Show API level guide
-if [[ ! $2 ]] && [ $1 == "-hh" ]; then
-	apiguide
-	exit 0
-elif [[ ! $2 ]] && [[ $1 != "" ]] && [ ! $(echo $1 | grep 'z') ]; then
-	showhelp 1
-	exit 0
-else
-	api_level=$2
 fi
 
-# Process
-if [[ "$3" != "" ]]; then
-	bootclass="$3"
+# Check for valid API level
+if [[ $api == 0 ]]; then
+	echo "Error: Please specify a valid API level."
+	show_help
+	exit 0
 fi
+
+# Deodex!
 for processDir in ${processDirList[@]}; do
 	echo ""
 	echo "Entering $processDir"
 	echo "************************"
-	cd $rootdir/triage/$processDir
+	cd "$rootdir/triage/$processDir"
 	for f in $(ls *.odex); do
-		deodex_file $f
+		deodex $f
 	done
-	echo ""
 	echo "Zipaligning APKs"
-	for d in $(ls *.apk);do
-		echo "- $d"
+	for d in *.apk; do
 		zpln $d
 	done
 done
 
-# Remove temp fw jar
-echo
-if [ -e $rootdir/triage/framework/java.awt.jar ]; then
-	rm $rootdir/triage/framework/java.awt.jar
-fi
-
 echo "Done."
-echo ""
 IFS=$SAVEIFS
 exit 0
